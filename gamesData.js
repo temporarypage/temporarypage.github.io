cd37314df589ee7c48e7ca833c5a0ab901cdd387
temporarypage <127648363+temporarypage@users.noreply.github.com>
2024-10-07 06:46:59 -0400

    Create gamesData.js

---
+++ gamesData.js
@@ -0,0 +1,9 @@
+// gamesData.js
+// Format: gamedir:gamename:gamethumbnaillink:gamegif:fixing:sw (sw means it has offline support with sw.js) (optional)
+// Use SaveAllResources, replace No Content blobs or wasms by going to the link and replacing.
+const games = [
+  "../games_FILE/cookie/index.html:Cookie Clicker:../images/cookie-clk.png:../images/cookie-clk.gif",
+  "../games_FILE/burritobison/burritobison.html:Burrito Bison:../images/burritobison.png:../images/burritobison.gif",
+  "../games_FILE/drivemad/drivemad.html:Drivemad:../images/drivemad.png",
+  "../games_FILE/GetawayShootout/index.html:Getaway Shootout:../images/getaway_shootout.png:../images/getaway.gif"
+];
